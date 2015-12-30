17add9925f74567b65572bacef7141eaccad0daf
murry <murry@localhost.localdomain>
2015-12-31 00:30:26 +0800

    set Header style

--- components/Header.js
+++ components/Header.js
@@ -10,16 +10,30 @@ export default class Header extends Component{
   render(){
     const {header} = this.props
     return (
-      <header>
-
-      </header>
+      <div style={styles.outer}>
+        <header style={styles.container}>
+          <div style={styles.item}>
+            {header.title}
+          </div>
+          <div style={styles.item}>
+            {header.subtitle}
+          </div>
+        </header>
+      </div>
     )
   }
 }
 
 const styles = {
-  container:{
-
+  outer:{
+    flex:1,
+    flexDirection:'row'
   },
-
+  container:{
+    flexDirection:'column',
+    height:30
+  },
+  item:{
+    flex:1
+  }
 }
