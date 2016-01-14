ec4c94c1fb1b4fe97fd14f8d293ff589d9c59231
bigMurry <hhdiao@siom.ac.cn>
2016-01-14 18:05:16 +0800

    add react-router

--- client/index.js
+++ client/index.js
@@ -3,14 +3,20 @@ import React from 'react'
 import {render} from 'react-dom'
 import configureStore from './store/configureStore'
 import {Provider} from 'react-redux'
+import {syncReduxAndRouter} from 'redux-simple-router'
+import history from 'history'
+import routes from './routes'
 import App from './containers/App'
 import './styles/index.css'
 
-const store = configureStore()
+//NOTE: react backend rendering, get the redux initial state
+const store = configureStore(window.__INITIAL_STATE__)
+
+syncReduxAndRouter(history, store, (state) => state.router)
 
 render(
   <Provider store = {store}>
-    <App />
+    <App history={history} routes={routes} />
   </Provider>,
   document.getElementById('root')
 )
