f1c1621e3025d125c41ba4b9535fdb8534ddb6cb
BigMurry <hhdiao@siom.ac.cn>
2016-01-19 21:43:16 +0800

    update Edit

--- client/containers/Edit/index.js
+++ client/containers/Edit/index.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import Button from '../../components/Button'
 import TiPlus from 'react-icons/lib/ti/plus'
-import classnames from 'classnames'
+import {connect} from 'react-redux'
+import cn from 'classnames'
 import styles from './style.scss'
 
 let{
   Component
 } = React
 
-export default class Edit extends Component{
+class Edit extends Component{
 
   constructor(props){
     super(props)
-    this.state = {
-      slidesCount:0
-    }
     this._onAdd = this._onAdd.bind(this)
+  }
+
+  static function transToString(obj){
+    let ret = ''
+    for(let key in obj){
+      if(obj.hasOwnProperty(key)){
+        ret +=`[${key}]${obj[key]}\n`
+      }
+    }
+    return ret
+  }
+
+  static defaultProps = {
+    slidesCount:0,
+    editPosition:0,
+    generalInfo:{
+      "title":'',
+      "subTitle":'',
+      "author":'',
+      "company":'',
+      "email":'',
+    },
+    slidesContent:[]
   }
 
   _onAdd(){
@@ -23,25 +44,36 @@ export default class Edit extends Component{
   }
 
   render(){
+
+    const {
+      slidesCount,
+      generalInfo,
+      slidesContent,
+      editPosition
+    } = this.props
+
     return (
       <div className={'content-container'}>
         <div className={styles.editContainer}>
           <div className={styles.generalBlock}>
             <p>General Information</p>
-            <textarea className={classnames(styles.textArea, styles.generalEdit)}>
+            <textarea className={cn(styles.textArea, styles.generalEdit)}>
+              {transToString(eneralInfo)}
             </textarea>
           </div>
           <div className={styles.displayBlock}>
             <Button onClick={this._onAdd}>
-              <TiPlus className={classnames('icon',styles.addIcon)}/>
+              <TiPlus className={cn('icon', styles.addIcon)}/>
             </Button>
             <Button className={styles.count}>
-              <div>{this.state.slidesCount}</div>
+              <div>{slidesCount}</div>
             </Button>
           </div>
           <div className={styles.slidesBlock}>
             <p>Slides Information</p>
-            <textarea className={classnames(styles.textArea,styles.slidesEdit)}></textarea>
+            <textarea className={cn(styles.textArea,styles.slidesEdit)}>
+              {transToString(slidesContent[editPosition])}
+            </textarea>
           </div>
         </div>
         <div className={styles.previewContainer}></div>
@@ -49,3 +81,14 @@ export default class Edit extends Component{
     )
   }
 }
+
+function mapStateToProps(state){
+  return {
+    slidesCount: state.edit.slidesCount,
+    editPosition: state.edit.position,
+    generalInfo: state.edit.generalInfo,
+    slidesContent: state.edit.slidesContent
+  }
+}
+
+export default connect(mapStateToProps)(Edit)
