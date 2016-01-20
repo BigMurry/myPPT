530eb7a3d3cb3ee34b8aa724cf5c10d22ed1e36e
BigMurry <hhdiao@siom.ac.cn>
2016-01-20 23:35:10 +0800

    modify edit storage

--- client/actions/edit/storage.js
+++ client/actions/edit/storage.js
@@ -11,15 +11,3 @@ export function saveSlidesInfo(){
     type: ActionTypes.SAVE_SLIDES_INFO
   }
 }
-
-export function changeGeneralInfo(){
-  return {
-    type: ActionTypes.CHANGE_GENERAL_INFO
-  }
-}
-
-export function changeSlidesContent(){
-  return {
-    type: ActionTypes.CHANGE_SLIDES_CONTENT
-  }
-}
