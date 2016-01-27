d2535cdb177eb5300e1deaee91bfc8ba7afa1baa
BigMurry <hhdiao@siom.ac.cn>
2016-01-27 23:29:13 +0800

    modify button test

--- __test__/client/components-spec.js
+++ __test__/client/components-spec.js
@@ -1,17 +1,52 @@
 import React from 'react'
-import TU from 'react-addons-test-utils'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
 
 import Button from '../../client/components/Button'
 
-describe('react component functionality', function(){
-  it('Button', function(){
-    let btn = TU.renderIntoDocument(
-      <Button>Button</Button>
+describe('react component functionality', () => {
+  let handlers = {
+    click: (e) => {
+      console.log('clicked')
+    }
+  }
+
+  it('TestUtils', () => {
+    expect(TestUtils).not.toBe(undefined)
+    expect(TestUtils.renderIntoDocument).not.toBe(undefined)
+    expect(TestUtils).not.toBe(null)
+    expect(TestUtils.renderIntoDocument).not.toBe(null)
+  })
+
+  it('Disabled Button', () => {
+    let btn = TestUtils.renderIntoDocument(
+      <Button disabled={true} onClick={handlers.click}>Button to click</Button>
     )
-    let nodes = TU.scryRenderedDOMComponentsWithTag(
+    let btnNode = ReactDOM.findDOMNode(btn)
+    expect(btnNode.textContent).toEqual('Button to click')
+
+    let btnEl = TestUtils.findRenderedDOMComponentWithTag(
       btn, 'button'
     )
 
-    expect(nodes.length).toEqual(1)
+    expect(TestUtils.isDOMComponent(btnEl)).toBe(true)
+    expect(btnEl.disabled).toBe(true)
+
+    spyOn(handlers, 'click').and.callThrough()
+    TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(
+      btn, 'button'
+    ))
+    expect(handlers.click.calls.any()).toBe(false)
+  })
+
+  it('enabled button', () => {
+    let btn = TestUtils.renderIntoDocument(
+      <Button disabled={false} onClick={handlers.click}>Enabled Button</Button>
+    )
+    spyOn(handlers,'click').and.callThrough()
+    TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithTag(
+      btn, 'button'
+    ))
+    expect(handlers.click.calls.any()).toBe(true)
   })
 })
