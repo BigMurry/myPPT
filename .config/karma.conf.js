51198d833477b644b1a2a605ac7bfdc527767755
BigMurry <hhdiao@siom.ac.cn>
2016-01-29 08:30:10 +0800

    rewrite test script in package.json

--- .config/karma.conf.js
+++ .config/karma.conf.js
@@ -75,15 +75,6 @@ module.exports = function (config) {
             test:/\.jsx?$/,
             exclude:/node_modules/,
             loader:'babel',
-            query:{
-              presets:[
-                "es2015",
-                "react",
-                "stage-0"
-              ],
-              //ignore global babelrc config
-              babelrc: false,
-            },
           }
         ]
       },
--- .config/test.webpack.config.js
+++ .config/test.webpack.config.js
@@ -14,14 +14,6 @@ module.exports = {
         test:/\.jsx?$/,
         exclude:/node_modules/,
         loader:'babel',
-        query:{
-          presets:[
-            "es2015",
-            "stage-0"
-          ],
-          //ignore global babelrc config
-          babelrc: false,
-        }
       }
     ]
   },
