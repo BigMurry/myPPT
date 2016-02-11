34f554d2137613dd9b98578312a45b83e54cfd14
BigMurry <hhdiao@siom.ac.cn>
2016-02-11 20:01:30 +0800

    change layout style

--- client/layouts/MainLayout.js
+++ client/layouts/MainLayout.js
@@ -1,25 +1,31 @@
 import React from 'react'
 
-export default function MainLayout(props){
-  return (
-    <div className={'layout'}>
-      <div style={styles.layoutTitle}>Online PPT</div>
-      {props.content}
-      <div style={styles.layoutFoot}>Copyright (c) Murry.Diao</div>
-    </div>
-  )
-}
+export default class MainLayout extends React.Component{
 
-const styles = {
-  layoutTitle:{
-    fontSize:40,
-    color:'rgb(147, 152, 124)'
-  },
-  layoutFoot:{
-    position:'fixed',
-    bottom:20,
-    color:'rgb(185, 194, 235)'
+  constructor(props){
+    super(props)
+    this._renderHead = this._renderHead.bind(this)
   }
+
+  _renderHead(){
+    return(
+      <div className={'layout-head'}>
+        <div className={'layout-title'}>Online PPT</div>
+        <div className={'layout-menu'}>Menu</div>
+      </div>
+    )
+  }
+
+  render(){
+    return (
+      <div className={'layout'}>
+        {this._renderHead()}
+        {this.props.content}
+        <div className={'layout-foot'}>Copyright (c) Murry.Diao</div>
+      </div>
+    )
+  }
+
 }
 
 MainLayout.propTypes = {
