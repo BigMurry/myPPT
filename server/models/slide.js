93a4d0806b3675ebcc57d62a46e45c1439add105
bigMurry <hhdiao@siom.ac.cn>
2016-02-16 17:51:52 +0800

    update slide model

---
+++ server/models/slide.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+const _DefaultSelect = ''
+const _DefaultOrder = ''
+
+//NOTE: B => Biography; A => Acknowlegement
+const SlideSchema = new Schema({
+  name:{type:String, default:'', trim:true},
+  creator:{type: Schema.ObjectId, ref: 'User'},
+  version:{type: String, default:'0.0.0', trim: true},
+  keywords:{type: String, default:'', trim: true},
+  desrciption:{type: String, default:'', trim: true},
+  content:{type: String, default:'', trim: true},
+  theme:{},
+  B:{type: Schema.ObjectId, ref:'Slide'},
+  A:{type: Schema.ObjectId, ref:'Slide'},
+  refs:[{
+    title:{type: String, default:'', trim: true},
+    description:{type:String, default:'', trim: true},
+    slide:{type: Schema.ObjectId, ref:'Slide'}
+  }]
+})
+
+mongoose.model('Slide', SlideSchema)
